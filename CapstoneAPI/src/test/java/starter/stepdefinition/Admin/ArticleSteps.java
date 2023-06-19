4b7bbf12b6c2d940b18e028c4beaafe75c14bae4
Arbecco <ibeccocode@gmail.com>
2023-06-19 13:05:06 +0700

    add admin article index, index param, delete, [WIP] create

--- CapstoneAPI/src/test/java/starter/stepdefinition/Admin/ArticleSteps.java
+++ CapstoneAPI/src/test/java/starter/stepdefinition/Admin/ArticleSteps.java
@@ -10,6 +10,7 @@ public class ArticleSteps {
     @Steps
     Article article;
 
+    // delete article
     @Given("I set admin delete article endpoint with {string}")
     public void iSetAdminDeleteArticleEndpointWith(String uuid) {
         article.setDeleteAdminArticleEndpoint(uuid);
@@ -24,5 +25,38 @@ public class ArticleSteps {
     public void iReceiveAdminArticleStatusCodeResponse(String code) {
         int statusCode = Integer.parseInt(code);
         article.verifyDeleteAdminArticleResponseCode(statusCode);
+    }
+
+    // index article
+    @Given("I set admin get all articles endpoint")
+    public void iSetAdminGetAllArticlesEndpoint() {
+        article.setIndexAdminArticlesEndpoint();
+    }
+
+    @When("I send GET admin get all articles request")
+    public void iSendGETAdminGetAllArticlesRequest() {
+        article.sendIndexAdminArticlesRequest();
+    }
+
+    // index article param
+    @Given("I set admin get all articles endpoint with query {string}")
+    public void iSetAdminGetAllArticlesEndpointWithQuery(String param) {
+        article.setIndexParamAdminArticlesEndpoint(param);
+    }
+
+    @When("I send GET admin get all articles with query param request")
+    public void iSendGETAdminGetAllArticlesWithQueryParamRequest() {
+        article.sendIndexParamAdminArticlesRequest();
+    }
+
+    // create
+    @Given("I set admin create article endpoint")
+    public void iSetAdminCreateArticleEndpoint() {
+        article.setAdminCreateArticleEndpoint();
+    }
+
+    @When("I send POST admin create article request")
+    public void iSendPOSTAdminCreateArticleRequest(io.cucumber.datatable.DataTable dataTable) {
+        article.sendAdminCreateArticleRequest(dataTable);
     }
 }
