429fc4218d320ff2d13514c9e446a7ca72041026
Arbecco <ibeccocode@gmail.com>
2023-06-26 22:00:05 +0700

    test API CI/CD for user login

--- CapstoneAPI/src/test/java/starter/CucumberTestSuite.java
+++ CapstoneAPI/src/test/java/starter/CucumberTestSuite.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         plugin = {"pretty"},
-        features = "src/test/resources/ApiAdmin"
+        features = "src/test/resources/ApiUser/1_login.feature"
 )
 public class CucumberTestSuite {
 
