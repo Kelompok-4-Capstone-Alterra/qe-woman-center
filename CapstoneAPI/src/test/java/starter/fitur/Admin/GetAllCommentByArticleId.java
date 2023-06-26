f57e991dfb7b774108320e1d5bf6123c707db941
alimustofa19 <83563912+alimustofa19@users.noreply.github.com>
2023-06-27 06:03:56 +0700

    ci-cd get all comment by article id

--- CapstoneAPI/src/test/java/starter/fitur/Admin/GetAllCommentByArticleId.java
+++ CapstoneAPI/src/test/java/starter/fitur/Admin/GetAllCommentByArticleId.java
@@ -9,7 +9,7 @@ public class GetAllCommentByArticleId {
     protected static String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGdtYWlsLmNvbSIsInVzZXJuYW1lIjoiYWRtaW4iLCJleHAiOjE2ODc5MjExMjN9.emN-gYe3TUSMSvFvLRXFur0tMwwQSeW82SA9sgCs6Ho";
 
     public String setGetAllCommentByArticleIdWithValidEndpoint(){
-        return url + "/admin/articles/d180f788-1343-11ee-a828-0242ac170003/comments";
+        return url + "/admin/articles/ad9e8915-1332-11ee-a828-0242ac170003/comments";
     }
     public String setGetAllCommentByArticleIdWithInvalidEndpoint(){
         return url + "/admn/articles/5819b9a2-0cd8-11ee-8713-0242ac1d0003/comments";
